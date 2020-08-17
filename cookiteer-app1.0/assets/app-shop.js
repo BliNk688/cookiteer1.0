6a1276b4b212e0519a804aab83582d03564135c5
luckyllm <1259129183@qq.com>
2020-08-17 20:25:46 +0800

    lumin app-shop commit second

---
+++ cookiteer-app1.0/assets/app-shop.js
@@ -0,0 +1,10 @@
+
+	function product_block(){
+	let block=document.getElementById("block");
+	block.classList.remove("product_list");
+	}
+	function product_list(){
+		
+	let block=document.getElementById("block");
+	block.classList.add("product_list");
+	}
